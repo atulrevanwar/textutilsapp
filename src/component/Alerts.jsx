968f9ef4ce623e5f778518c84ec0ec93d9b5f90e
Atul Revanwar <arevanwar@gmail.com>
2024-02-01 11:15:54 +0530

    updated  file

--- src/component/Alerts.jsx
+++ src/component/Alerts.jsx
@@ -4,9 +4,9 @@ export default function Alerts(props) {
   return (
         props.alerts && <div className={`alert alert-${props.alerts.type} alert-dismissible fade show`} role="alert">
             {props.alerts.type} : {props.alerts.msg} 
-            <button type="button" className="btn-close mx-3" data-bs-dismiss="alert" aria-label="Close">
+            {/* <button type="button" className="btn-close mx-3" data-bs-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
-            </button>
+            </button> */}
         </div>
     
   )
